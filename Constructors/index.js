9cbadbd5c4e3588c54756e75f207e21c2db6c4d7
Nuwan Vidanapathiranage <nuwanpvp@gmail.com>
2024-02-21 22:01:11 +0530

    Extend Player class with sayHello method and prototype inheritance
    Define a Player class with name and marker properties
    Implement a constructor to initialize name and marker for each player instance
    Add a sayName method to the Player class to log the player's name
    Create instances of the Player class for player1 and player2 with different names and markers
    Verify prototype inheritance using Object.getPrototypeOf and proto
    Add a sayHello method to the Player.prototype to allow all instances of Player class to access it
    Test sayHello method functionality by calling it on player1 and player2 instances
    Verify prototype chain and inheritance using hasOwnProperty method

--- Constructors/index.js
+++ Constructors/index.js
@@ -28,3 +28,17 @@ Player.prototype.sayHello = function() {
  
  player1.sayHello(); // logs "Hello, I'm a player!"
  player2.sayHello(); // logs "Hello, I'm a player!"
+
+ console.log(player1.__proto__ === Player.prototype) // returns true
+ console.log(player2.__proto__ === Player.prototype); // returns true
+
+ // Player.prototype.__proto__
+Object.getPrototypeOf(Player.prototype) === Object.prototype; // true
+
+// Output may slightly differ based on the browser
+player1.valueOf(); // Output: Object { name: "steve", marker: "X", sayName: sayName() }
+
+player1.hasOwnProperty('valueOf'); // false
+Object.prototype.hasOwnProperty('valueOf'); // true
+
+Object.prototype.hasOwnProperty('hasOwnProperty'); // true
